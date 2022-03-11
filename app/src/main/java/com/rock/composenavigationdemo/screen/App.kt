4178413fa1643821f77f80a120e65dc2eef4e5cc
llwdslal <466186888@qq.com>
2022-03-11 17:18:12 +0800

    deeplink      launchSingleTop

--- app/src/main/java/com/rock/composenavigationdemo/screen/App.kt
+++ app/src/main/java/com/rock/composenavigationdemo/screen/App.kt
@@ -1,13 +1,10 @@
 package com.rock.composenavigationdemo.screen
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NamedNavArgument
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 
 @Composable
 fun App(){
@@ -22,7 +19,11 @@ fun App(){
             SecondScreen(navController)
         }
 
-        composable(route = Screen.Third.route, arguments = Screen.Third.arguments) {
+        composable(route = Screen.Third.route, arguments = Screen.Third.arguments,
+            deepLinks = listOf(
+                navDeepLink { uriPattern = "test://nav.test.thirdscreen/{arg1}?arg2={arg2}"  }
+            )
+        ) {
             val arg1 = Screen.Third.getArg1(it)
             val arg2 = Screen.Third.getArg2(it)
             ThirdScreen(navController,arg1,arg2)
--- app/src/main/java/com/rock/composenavigationdemo/screen/SecondScreen.kt
+++ app/src/main/java/com/rock/composenavigationdemo/screen/SecondScreen.kt
@@ -1,18 +1,25 @@
 package com.rock.composenavigationdemo.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+private const val TAG = "SecondScreen"
 @Composable
 fun SecondScreen(navController: NavController){
+    //打印 back stack
+    Log.e(TAG, "back stack : ${navController.backQueue
+        //navController 第一个 NavBackStackEntry 是 NavGraph , 它的 route 是 null
+        .filter { it.destination.route  != null }  
+        .map { it.destination.route }}"
+    )
     Scaffold(
         topBar = { TopAppBar(
             title = { Text(text = "Second")},
@@ -28,6 +35,18 @@ fun SecondScreen(navController: NavController){
             Text(text = "SecondScreen")
             Button(onClick = { navController.navigate(Screen.Third.createRoute("So easy",100)) }) {
                 Text(text = "Go Third Screen")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = {
+                //SecondScreen 中 navigate 到 SecondScreen ，会创建一个新的 SecondScreen 放到 back 栈顶
+                //back 栈就会多个 SecondScreen 实例
+
+                navController.navigate(Screen.Second.route){
+                    //如果 back 栈顶的实例跟 navigate 方法指定的路由一样，不重新创建实例，只触发 recompose
+                    launchSingleTop = true
+                }
+            }) {
+                Text(text = "Launch Self")
             }
         }
     }
